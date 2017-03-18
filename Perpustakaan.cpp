782d1b0a1823acc4622b72f3fbbf143af2f54d59
Zakuby <zakuby02@gmail.com>
2017-03-19 05:06:23 +0700

    Tugas Praktikum Simplify

--- Perpustakaan.cpp
+++ Perpustakaan.cpp
@@ -21,23 +21,13 @@ private:
 	string kategori;
 public:
 	
-	void setNamaBuku(string s) {
-		nama_buku = s;
-	}
-	void setPengarang(string s) {
-		pengarang = s;
-	}
-	void setPenerbit(string s) {
-		penerbit = s;
-	}
-	void setTahunTerbit(int i) {
-		tahun_terbit = i;
-	}	
-	void setTempatTerbit(string s) {
-		tempat_terbit = s;
-	}
-	void setKategori(string s) {
-		kategori = s;
+	void setBuku(string nama, string pengarang, string penerbit, string tempat_terbit, int tahun_terbit, string kategori){
+		this->nama_buku = nama;
+		this->pengarang = pengarang;
+		this->penerbit = penerbit;
+		this->tempat_terbit = tempat_terbit;
+		this->tahun_terbit = tahun_terbit;
+		this->kategori = kategori;
 	}
 	
 	string getNamaBuku() {
@@ -133,12 +123,7 @@ int main() {
 				cout<<"Masukkan Tahun Terbit Buku \t: ";
 				cin>> tahun_terbit;
 				fflush(stdin);
-				teknologi[n_tekno].setNamaBuku(nama_buku);
-				teknologi[n_tekno].setPengarang(pengarang);
-				teknologi[n_tekno].setPenerbit(penerbit);
-				teknologi[n_tekno].setTempatTerbit(tempat_terbit);
-				teknologi[n_tekno].setTahunTerbit(tahun_terbit);
-				teknologi[n_tekno].setKategori(cat.teknologi);
+				teknologi[n_tekno].setBuku(nama_buku, pengarang, penerbit, tempat_terbit, tahun_terbit, cat.teknologi);
 				n_tekno++;
 			}else if(pilihan_kategori == 4){
 				cout<<"Masukkan Nama Buku \t\t: ";
@@ -156,12 +141,7 @@ int main() {
 				cout<<"Masukkan Tahun Terbit Buku \t: ";
 				cin>> tahun_terbit;
 				fflush(stdin);
-				agama[n_agama].setNamaBuku(nama_buku);
-				agama[n_agama].setPengarang(pengarang);
-				agama[n_agama].setPenerbit(penerbit);
-				agama[n_agama].setTempatTerbit(tempat_terbit);
-				agama[n_agama].setTahunTerbit(tahun_terbit);
-				agama[n_agama].setKategori(cat.agama);
+				agama[n_agama].setBuku(nama_buku, pengarang, penerbit, tempat_terbit, tahun_terbit, cat.agama);
 				n_agama++;
 			}else if(pilihan_kategori == 7){
 				cout<<"Masukkan Nama Buku \t\t: ";
@@ -179,12 +159,7 @@ int main() {
 				cout<<"Masukkan Tahun Terbit Buku \t: ";
 				cin>> tahun_terbit;
 				fflush(stdin);
-				fiksi[n_fis].setNamaBuku(nama_buku);
-				fiksi[n_fis].setPengarang(pengarang);
-				fiksi[n_fis].setPenerbit(penerbit);
-				fiksi[n_fis].setTempatTerbit(tempat_terbit);
-				fiksi[n_fis].setTahunTerbit(tahun_terbit);
-				fiksi[n_fis].setKategori(cat.fiksi);
+				fiksi[n_fis].setBuku(nama_buku, pengarang, penerbit, tempat_terbit, tahun_terbit, cat.fiksi);
 				n_fis++;
 			}else if(pilihan_kategori == 2){
 				cout<<"Masukkan Nama Buku \t\t: ";
@@ -202,12 +177,7 @@ int main() {
 				cout<<"Masukkan Tahun Terbit Buku \t: ";
 				cin>> tahun_terbit;
 				fflush(stdin);
-				filsafat[n_fil].setNamaBuku(nama_buku);
-				filsafat[n_fil].setPengarang(pengarang);
-				filsafat[n_fil].setPenerbit(penerbit);
-				filsafat[n_fil].setTempatTerbit(tempat_terbit);
-				filsafat[n_fil].setTahunTerbit(tahun_terbit);
-				filsafat[n_fil].setKategori(cat.filsafat);
+				filsafat[n_fil].setBuku(nama_buku, pengarang, penerbit, tempat_terbit, tahun_terbit, cat.filsafat);
 				n_fil++;
 			}else if(pilihan_kategori == 6){
 				cout<<"Masukkan Nama Buku \t\t: ";
@@ -225,12 +195,7 @@ int main() {
 				cout<<"Masukkan Tahun Terbit Buku \t: ";
 				cin>> tahun_terbit;
 				fflush(stdin);
-				politik[n_poli].setNamaBuku(nama_buku);
-				politik[n_poli].setPengarang(pengarang);
-				politik[n_poli].setPenerbit(penerbit);
-				politik[n_poli].setTempatTerbit(tempat_terbit);
-				politik[n_poli].setTahunTerbit(tahun_terbit);
-				politik[n_poli].setKategori(cat.politik);
+				politik[n_poli].setBuku(nama_buku, pengarang, penerbit, tempat_terbit, tahun_terbit, cat.politik);
 				n_poli++;
 			}else if(pilihan_kategori == 3){
 				cout<<"Masukkan Nama Buku \t\t: ";
@@ -248,12 +213,7 @@ int main() {
 				cout<<"Masukkan Tahun Terbit Buku \t: ";
 				cin>> tahun_terbit;
 				fflush(stdin);
-				sejarah[n_sej].setNamaBuku(nama_buku);
-				sejarah[n_sej].setPengarang(pengarang);
-				sejarah[n_sej].setPenerbit(penerbit);
-				sejarah[n_sej].setTempatTerbit(tempat_terbit);
-				sejarah[n_sej].setTahunTerbit(tahun_terbit);
-				sejarah[n_sej].setKategori(cat.sejarah);
+				sejarah[n_sej].setBuku(nama_buku, pengarang, penerbit, tempat_terbit, tahun_terbit, cat.sejarah);
 				n_sej++;
 			}else if(pilihan_kategori == 5){
 				cout<<"Masukkan Nama Buku \t\t: ";
@@ -271,12 +231,7 @@ int main() {
 				cout<<"Masukkan Tahun Terbit Buku \t: ";
 				cin>> tahun_terbit;
 				fflush(stdin);
-				psikologi[n_psik].setNamaBuku(nama_buku);
-				psikologi[n_psik].setPengarang(pengarang);
-				psikologi[n_psik].setPenerbit(penerbit);
-				psikologi[n_psik].setTempatTerbit(tempat_terbit);
-				psikologi[n_psik].setTahunTerbit(tahun_terbit);
-				psikologi[n_psik].setKategori(cat.psikologi);
+				psikologi[n_psik].setBuku(nama_buku, pengarang, penerbit, tempat_terbit, tahun_terbit, cat.psikologi);
 				n_psik++;
 			}
 		}
